59e89df14ccb59a714d11f3cfa76cf45fc245571
Christoffel Kweknoto <emperorchrisz@gmail.com>
2021-05-03 15:16:48 +0700

    My first website

---
+++ editing/skrip.js
@@ -0,0 +1,11 @@
+alert("klik ok untuk melanjutkan")
+const namadepan = prompt("tulis nama depan anda");
+const namabelakang = prompt("tulis nama belakang");
+const pengunjung = {
+    nama: {
+        depan: namadepan,
+        belakang: namabelakang,
+    },
+ };
+console.log(pengunjung)
+alert("selamat datang "+ namadepan +" " + namabelakang)
